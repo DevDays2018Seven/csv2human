4cda94b354e64c72fb973f907ed34d7bc09861b6
Andreas Pfohl <mail@apfohl.com>
2019-05-20 17:02:11 +0200

    moved distribution implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HeadersController } from './headers/headers.controller';
 import { CsvService } from './shared/csv.service';
-import { ColumnsController } from './columns/columns.controller';
 import { AverageController } from './average/average.controller';
 import { PlotController } from './plot/plot.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, HeadersController, ColumnsController, AverageController, PlotController],
+  controllers: [AppController, HeadersController, AverageController, PlotController],
   providers: [AppService, CsvService],
 })
 export class AppModule {}
--- src/plot/plot.controller.ts
+++ src/plot/plot.controller.ts
@@ -8,14 +8,6 @@ export class PlotController {
 
   public constructor(private readonly csvService: CsvService) {}
 
-  @Get('distribution/:name/:count')
-  public column(
-    @Param('name') name: string,
-    @Param('count') count: string,
-  ): Observable<string[]> {
-    return this.csvService.getColumn(name);
-  }
-
   @Get('scatter/:x/:y')
   public scatter(
     @Param('x') xcolumn: string,
@@ -24,6 +16,38 @@ export class PlotController {
     return this.csvService.getValuePairs(xcolumn, ycolumn).pipe(
       map(array => array.map(o => ({ x: Number(o.ping), y: Number(o.pong) }))),
       map(o => ({ data: o })),
+    );
+  }
+
+  @Get('distribution/:name/:count')
+  public distribution(
+    @Param('name') name: string,
+    @Param('count') count: string,
+  ): Observable<{ data: number[], labels: string[] }> {
+    const numericalCount: number = Number(count);
+
+    return this.csvService.getColumn(name).pipe(
+      map(value => value.map(entry => Number(entry))),
+      map(value => {
+        const data: number[] = [...new Array<number>(numericalCount)].map(_ => 0);
+
+        const filterNaN = value.filter(v => !isNaN(v));
+
+        const max: number = Math.max(...filterNaN);
+        const min: number = Math.min(...filterNaN);
+
+        const width = (max - min) / numericalCount;
+
+        filterNaN.forEach(entry => {
+          data[Math.min(numericalCount - 1, Math.floor((entry - min) / width))]++;
+        });
+
+        const labels: string[] = [...new Array(numericalCount)].map((_, index) => {
+          return `< ${Math.floor((index + 1) * width)}`;
+        });
+
+        return {data, labels};
+      }),
     );
   }
 }
