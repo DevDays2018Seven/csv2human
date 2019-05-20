aed641bd00b2b405aa39ec1243439f73382eab7f
Eric Schubert <eric.schubert@xitaso.com>
2019-05-20 15:26:27 +0200

    added three average types

--- src/average/average.controller.ts
+++ src/average/average.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Query, BadRequestException } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { CsvService } from '../shared/csv.service';
-import { Observable, of } from 'rxjs';
 
 @Controller('average')
 export class AverageController {
@@ -9,16 +10,48 @@ export class AverageController {
   public constructor(private readonly csvService: CsvService) { }
 
   @Get()
-  // public async average(@Param('column') column: string): Promise<number> {
   public average(@Query('column') column: string, @Query('type') type: string): Observable<number> {
     if (!column) {
       throw new BadRequestException('Column name is not specified.');
     }
 
-    return of(AverageController.calculateAverage([1, 2, 2, 3, 4, 7, 9], 'arithmetic-mean'));
+    return this.csvService.getColumn(column).pipe(
+      map(value => value.map(entry => Number(entry))),
+      map(value => AverageController.calculateAverage(value, type)),
+    );
   }
 
   private static calculateAverage(numbers: number[], type: string): number {
-    return 43;
+    const filteredNumbers: number[] = numbers.filter(value => !isNaN(value));
+
+    if (filteredNumbers.length === 0) {
+      throw new BadRequestException(`Column contains only non numeric values.`);
+    }
+
+    // Default average type
+    if (type === undefined) { type = 'arithmetic-mean'; }
+
+    const dict: { [key: string]: (numbers: number[]) => number; } = {};
+    dict['arithmetic-mean'] = AverageController.calculateArithmeticMean;
+    dict.median = AverageController.calculateMedian;
+    dict['mid-range'] = AverageController.calculateMidRange;
+
+    if (Object.keys(dict).filter(value => value === type).length === 0) {
+      throw new BadRequestException(`Type ${type} is not a known average type.`);
+    }
+
+    return dict[type](filteredNumbers);
+  }
+
+  private static calculateArithmeticMean(numbers: number[]): number {
+    return numbers.reduce((prev, cur) => prev + cur) / numbers.length;
+  }
+
+  private static calculateMedian(numbers: number[]): number {
+    return numbers.slice(numbers.length / 2)[0];
+  }
+
+  private static calculateMidRange(numbers: number[]): number {
+    return (Math.max(...numbers) + Math.min(...numbers)) / 2;
   }
 }
