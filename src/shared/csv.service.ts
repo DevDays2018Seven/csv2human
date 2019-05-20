04884a44d9718e406018960aff939e638e7d7c6b
Eric Schubert <eric.schubert@xitaso.com>
2019-05-20 12:57:41 +0200

    load csv via pattern

--- src/shared/csv.service.ts
+++ src/shared/csv.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { createReadStream } from 'fs';
+import * as glob from 'glob';
 import * as csvParser from 'csv-parser';
 
 @Injectable()
@@ -15,11 +16,16 @@ export class CsvService {
     if (this.csv.length !== 0) { return Promise.resolve((this.csv)); }
 
     return new Promise<object[]>((resolve, reject) => {
-      createReadStream('resources/scotch_review.csv')
-        .pipe(csvParser())
-        .on('data', chunk => this.csv.push(chunk))
-        .on('end', () => resolve(this.csv))
-        .on('error', err => reject(err));
+
+      glob('resources/*.csv', (error, files) => {
+        if (error) { reject(error); }
+
+        createReadStream(files[0])
+          .pipe(csvParser())
+          .on('data', chunk => this.csv.push(chunk))
+          .on('end', () => resolve(this.csv))
+          .on('error', err => reject(err));
+      });
     });
   }
 }
